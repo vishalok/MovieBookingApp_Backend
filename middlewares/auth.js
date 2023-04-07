894e646225c1ec4656c7234584e2823a66402c88
vishalok <vrsafetech@gmail.com>
2023-04-07 16:24:09 +0530

    gmail integration

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/users.models');
+const Theatre = require('../models/theatre.model');
+
 async function verifyToken(req, res, next){
     const userToken = req.headers['x-access-token'];
     if(!userToken){
@@ -43,7 +45,43 @@ async function isAdmin(req, res, next){
     next();
 }
 
+async function isAdminOrClient(req, res, next){
+    const id = req._id;
+
+    const user = await User.findOne({
+        _id: id
+    })
+
+    if(user && user.userStatus === 'APPROVED'){
+        if(user.userType === 'ADMIN'){
+            next();
+        }else if(user.userType === 'CLIENT'){
+            const theatre = await Theatre.findOne({
+                ownerId: id,
+                _id: req.params.id
+            });
+            if(!theatre){
+                return res.status(400).send({
+                    msg :`This user ${id} is not the owner of this theatre ${req.params.id}`
+                })
+            }else{
+                next();
+            }
+
+        }else{
+            return res.status(400).send({
+                msg :"User is not admin not any client"
+            })
+        }
+    }else{
+        return res.status(400).send({
+            msg :"User is not approved"
+        })
+    }
+}
+
 module.exports = {
     verifyToken,
-    isAdmin
+    isAdmin,
+    isAdminOrClient
 }
